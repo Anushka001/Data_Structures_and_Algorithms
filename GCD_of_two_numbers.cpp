463845f46c64834cda59ce1d59962ffc72eb97a7
Anushka Ayachit <68822433+Anushka001@users.noreply.github.com>
2021-12-23 22:12:31 +0530

    Add files via upload

---
+++ GCD_of_two_numbers.cpp
@@ -0,0 +1,31 @@
+/* Finding Greatest Common Divisor using Euclid's Algorithm*/
+
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+int main()
+{
+    int a, b;
+    cout << "Enter a and b:" << endl;
+    cin >> a >> b;
+    int ans = gcd(a, b);
+
+    cout << "GCD of " << a << " and " << b << " is: " << ans << endl;
+    return 0;
+}
